249d26756d8124f1f4a6e25ac601b14b76a21ef2
mottapd <brunovellego@hotmail.com>
2018-09-29 10:34:03 -0300

    adicionando preco

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -21,6 +23,8 @@ public class Sessao {
 	private Sala sala;
 	private LocalTime horario;
 	
+	private BigDecimal preco = BigDecimal.ZERO;
+	
 	public Sessao() {
 		
 	}
@@ -29,6 +33,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 	
 	public LocalTime getHorarioTermino() {
@@ -65,5 +70,13 @@ public class Sessao {
 
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
+	}
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 }
